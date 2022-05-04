32b6c2c422ad4a4d4e5770c36dc29f1e342f2b2d
01000001kuma <a_illera@yahoo.com>
2022-05-04 18:22:41 +0200

    class

--- app.js
+++ app.js
@@ -4,4 +4,34 @@ require("dotenv").config();
 
 const app = express();
 
-// Build the server here
+app.use(express.static("public"));
+
+app.get("/", (request, res) =>
+    res.sendFile("__dirname + views/baseball.html")
+);
+
+app.get("/", (request, res) =>
+    res.sendFile("__dirname + views/hockey.html")
+);
+
+app.get("/", (request, res) =>
+    res.sendFile("__dirname + views/basketball.html")
+);
+
+
+
+// // Build the server here
+// app.get("/", (request, response) =>
+//   response.sendFile(__dirname + "/views/index.html")
+// );
+
+// app.get("/homepage", (req, res) => 
+//     res.sendFile(__dirname + "/views/home.html")
+// );
+
+// app.get("/about", (req, res) => 
+//     res.sendFile(__dirname + "/views/about.html")
+// );
+
+const PORT = 3050;
+app.listen(PORT, () => console.log('running on port 3000'));
